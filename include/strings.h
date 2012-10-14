fcc80af27943cf903f6e56f351bd75244b36a355
Kelvin Sherlock <ksherlock@gmail.com>
2012-10-14 19:17:18 -0400

    remove visibility checks.

--- include/strings.h
+++ include/strings.h
@@ -51,24 +51,17 @@ typedef	_BSD_SIZE_T_	size_t;
 #endif
 
 __BEGIN_DECLS
-//#if __BSD_VISIBLE || __POSIX_VISIBLE <= 200112
 int	 bcmp(const void *, const void *, size_t) __pure;	/* LEGACY */
 void	 bcopy(const void *, void *, size_t);			/* LEGACY */
 void	 bzero(void *, size_t);					/* LEGACY */
-//#endif
-//#if __XSI_VISIBLE
 int	 ffs(int) __pure2;
-//#endif
 
-//#if __BSD_VISIBLE || __POSIX_VISIBLE <= 200112
 char	*index(const char *, int) __pure;			/* LEGACY */
 char	*rindex(const char *, int) __pure;			/* LEGACY */
-//#endif
 int	 strcasecmp(const char *, const char *) __pure;
 int	 strncasecmp(const char *, const char *, size_t) __pure;
 
 #ifndef _ANSI_SOURCE
-/* __BSD_VISIBLE */
 
 int	 ffsl(long) __pure2;
 //int	 ffsll(long long) __pure2;
